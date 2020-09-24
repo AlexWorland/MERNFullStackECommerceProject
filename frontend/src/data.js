a66493da24113460483ebef08ce2a93ae060a873
alexworland <alexworland@gmail.com>
2020-09-24 15:53:58 -0700

    Extracts product list items to a file named data.js which holds an array of products.

---
+++ frontend/src/data.js
@@ -0,0 +1,31 @@
+export default {
+    products:[
+        {
+        name: 'Slim Shirt',
+        category: 'Shirts',
+        image: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10
+        },
+        {
+            name: 'Slim Pants',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 80,
+            brand: 'Amazon Basics',
+            rating: 3.8,
+            numReviews: 15
+        },
+        {
+            name: 'Slimmer Pants',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 90,
+            brand: 'Amazon Basics',
+            rating: 3.2,
+            numReviews: 25
+        },
+    ]
+}
