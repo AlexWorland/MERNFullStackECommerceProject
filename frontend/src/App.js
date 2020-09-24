1dcdfdbf25f7dc76e3cdb2e09199c8968bce2aa1
alexworland <alexworland@gmail.com>
2020-09-23 21:21:27 -0700

    Reformatted App.js for javascript

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open");
+  }
+
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open");
+  }
+
   return (
       <div className="grid-container">
         <header className="header">
           <div className="brand">
-            <button onClick="openMenu()">
+            <button onClick={openMenu}>
               &#9776;
             </button>
             <a href="index.html">full stack store</a>
@@ -20,7 +29,7 @@ function App() {
 
         <aside className="sidebar">
           <h3>Shopping Categories</h3>
-          <button className="sidebar-close-button" onClick="closeMenu()">x</button>
+          <button className="sidebar-close-button" onClick={closeMenu}>x</button>
           <ul>
             <li>
               <a href="index.html">Pants</a>
