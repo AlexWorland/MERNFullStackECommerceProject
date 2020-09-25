33a30a086b87704f98e03d002674065e935c7fc7
alexworland <alexworland@gmail.com>
2020-09-25 13:16:54 -0700

    Added redux state manager to handle informing the user if there is an error with retrieving data from the backend. Also to show the user a loading screen when finding a product.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,17 +2,25 @@ import React, {useEffect, useState} from 'react';
 // import data from "../data"; No longer necessary. Fetching data from backend.
 import {Link} from 'react-router-dom';
 import axios from 'axios';
+import {useDispatch, useSelector} from "react-redux";
+import {listProducts} from "../actions/productActions";
 
 function HomeScreen (props) {
 
     // Backend integration. Fetches data from backend.
-    const [products, setProducts] = useState([]);
+    // const [products, setProducts] = useState([]);
+    const productList = useSelector(state => state.productList)
+    const {products, loading, error} = productList;
+    const dispatch = useDispatch();
     useEffect(() => {
-        const fetchData = async () => {
-            const {data} = await axios.get("/api/products");
-            setProducts(data);
-        }
-        fetchData()
+        // const fetchData = async () => {
+        //     const {data} = await axios.get("/api/products");
+        //     setProducts(data);
+        // }
+        // fetchData()
+
+        dispatch(listProducts());
+
         return () => {
             //
         };
@@ -21,31 +29,36 @@ function HomeScreen (props) {
     // DEBUG
     console.log("This is the homescreen.js")
 
-    return (<ul className="products">
-        {
-            // For each product in products, display.
-            products.map(product =>
-                // use a key to make each li element unique.
-                <li key={product._id}>
-                    <div className="product">
-                        <Link to={'/product/' + product._id}>
-                            <img className="product-image" src="/images/d1.jpg" alt="product"/>
-                        </Link>
-                        <div className="product-name">
-                            <Link to={'/product/' + product._id}>{product.name}</Link>
-                        </div>
-                        <div className="product-brand">
-                            {product.brand}
-                        </div>
-                        <div className="product-price">
-                            ${product.price}
-                        </div>
-                        <div className="product-rating">
-                            {product.rating} Stars ({product.numReviews} Reviews)</div>
-                    </div>
-                </li>)
-        }
-    </ul>)
+    // Checks if currently loading, if theres an error, and if neither display list of products.
+    return loading ? <div>Loading...</div> :
+        error ? <div>{error}</div> :
+            // return (<ul className="products">
+            (<ul className="products">
+                {
+                    // For each product in products, display.
+                    products.map(product =>
+                        // use a key to make each li element unique.
+                        <li key={product._id}>
+                            <div className="product">
+                                <Link to={'/product/' + product._id}>
+                                    <img className="product-image" src="/images/d1.jpg" alt="product"/>
+                                </Link>
+                                <div className="product-name">
+                                    <Link to={'/product/' + product._id}>{product.name}</Link>
+                                </div>
+                                <div className="product-brand">
+                                    {product.brand}
+                                </div>
+                                <div className="product-price">
+                                    ${product.price}
+                                </div>
+                                <div className="product-rating">
+                                    {product.rating} Stars ({product.numReviews} Reviews)
+                                </div>
+                            </div>
+                        </li>)
+                }
+            </ul>)
 }
 
 export default HomeScreen;
